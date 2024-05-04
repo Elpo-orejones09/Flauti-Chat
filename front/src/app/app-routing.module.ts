cceab9ca823067818ff1819e19eb6c6c8da7332b
Allex44 <104902710+Allex44@users.noreply.github.com>
2024-05-04 17:55:09 +0200

    :)

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+//Aqui cargamos todas las rutas, no es necesario tener un archivo routing para cada componente tenemos un archivo de rutas a nivel global y las inyectamos
 const routes: Routes = [
-  {
+  { //Path to home
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  { //Path to registro
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then(m => m.RegistroComponentPageModule)
   },
   {
     path: '',
@@ -19,4 +23,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
--- front/src/app/registro/registro.component.ts
+++ front/src/app/registro/registro.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss'],
 })
-export class RegistroComponent  implements OnInit {
+export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {}
+
+  public redirectTo(url: String) {
+    this.router.navigate([url]);
+  }
+  ngOnInit() { }
 
 }
