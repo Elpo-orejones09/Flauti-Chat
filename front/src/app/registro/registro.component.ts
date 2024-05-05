19375e7c4b977adc8d7a9dfce5ed2a565b3e3c6e
Elpo-orejones09 <velazquezlopezr4@gmail.com>
2024-05-05 20:38:29 +0200

    cambios en el formulario

--- front/src/app/registro/registro.component.ts
+++ front/src/app/registro/registro.component.ts
@@ -14,6 +14,10 @@ export class RegistroComponent implements OnInit {
   public redirectTo(url: String) {
     this.router.navigate([url]);
   }
+
   ngOnInit() { }
 
+  registrarse(){
+    console.log("mostrar Registrar")
+  }
 }
