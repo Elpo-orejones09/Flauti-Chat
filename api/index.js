09372d99643c4d043dd910e52e9fbd4daaaf25ec
Elpo-orejones09 <velazquezlopezr4@gmail.com>
2024-05-15 17:58:59 +0200

    Cambios para registrar

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const router = express.Router();
-const connection = require('./database.js'); 
+const app = express(); 
+const connection = require('./database.js');
 
-router.get('/api/usuarios', (req, res) => {
+//firebase
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase/flauti-chat-firebase-adminsdk-maaom-eafd1e5fbd.json");
+
+//midle
+app.use(express.json());
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://flauti-chat-default-rtdb.europe-west1.firebasedatabase.app"
+});
+
+app.get('/api/usuarios', (req, res) => {
     connection.query('SELECT * FROM usuarios', (err, results) => {
         if (err) {
             console.error('Error al obtener usuarios:', err);
@@ -12,16 +24,43 @@ router.get('/api/usuarios', (req, res) => {
         res.json(results);
     });
 });
-router.post('/api/usuarios', (req, res) => {
+
+app.get('/api/usuarios/iniSesion', (req, res) => {
+
+})
+
+//crear usuarios
+app.post('/api/usuarios', (req, res) => {
     const nuevoUsuario = req.body;
-    connection.query('INSERT INTO usuario SET ?', nuevoUsuario, (err, results) => {
-        if (err) {
-            console.error('Error al crear usuario:', err);
+    admin.auth().createUser(nuevoUsuario)
+    .then((userRecord) => {
+        // La creación de usuario fue exitosa, ahora puedes insertar los datos en la base de datos
+        const usuarioBdd = {
+            nombre: userRecord.displayName,
+            email: userRecord.email,
+            fotoPerfil: userRecord.photoURL
+        };
+        connection.query('INSERT INTO usuarios SET ?', usuarioBdd, (err, results) => {
+            if (err) {
+                console.error('Error al insertar usuario en la base de datos:', err);
+                res.status(500).json({ error: 'Error al crear usuario' });
+                return;
+            }
+            console.log(userRecord);
+            res.json({ message: 'Usuario creado exitosamente', id: results.insertId });
+        });
+    })
+    .catch((error) => {
+        // La creación de usuario falló, maneja el error adecuadamente
+        if (error.code === 'auth/email-already-exists') {
+            res.status(400).json({ error: 'Ya hay una cuenta con ese correo electrónico' });
+        } else {
+            console.error('Error al crear usuario:', error);
             res.status(500).json({ error: 'Error al crear usuario' });
-            return;
         }
-        res.json({ message: 'Usuario creado exitosamente', id: results.insertId });
     });
 });
 
-module.exports = router;
+
+
+module.exports = app;
