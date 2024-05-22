04eb7b0ef92fad3ef425f8aff771b1d5273523df
Allex44 <104902710+Allex44@users.noreply.github.com>
2024-05-22 17:28:34 +0200

    No he hecho practicamente nada, Chatgipiti ha petado a las 18:30 aprox continuo que ahora tengo que ir a hacer una movida.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AuthService } from './services/auth.service';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +20,7 @@ import { AuthService } from './services/auth.service';
     IonicModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AuthService],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AuthService, provideFirebaseApp(() => initializeApp({"projectId":"flauti-chat","appId":"1:71228493507:web:5c010d94c37d57e62b9c76","databaseURL":"https://flauti-chat-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"flauti-chat.appspot.com","apiKey":"AIzaSyDafhFR7waF8lsv0nynPsHR77KPQ4gIuTE","authDomain":"flauti-chat.firebaseapp.com","messagingSenderId":"71228493507","measurementId":"G-DGE79KQPRV"})), provideAuth(() => getAuth()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
