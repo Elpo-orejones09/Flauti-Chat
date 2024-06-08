584e4243bd50223eeed8b6a7e50137176e16448b
Allex44 <104902710+Allex44@users.noreply.github.com>
2024-06-08 21:42:43 +0200

    muchas MUCHAS cosas

--- front/src/app/user-profile/user-profile.component.ts
+++ front/src/app/user-profile/user-profile.component.ts
@@ -10,11 +10,12 @@ import { PerfilService } from '../services/perfil.service';
 export class UserProfileComponent implements OnInit {
   usuario: any;
   publicaciones: any[] = []; // Adaptar para que se cargue dinámicamente si es necesario
-  numeroPublicaciones:number=0;
-  seguidores:number=0; 
-  seguidos:number=0;
+  mostrarFormularioEdicion = false; // Controla la visibilidad del formulario
+  numeroPublicaciones: number = 0;
+  seguidores: number = 0;
+  seguidos: number = 0;
 
-  constructor(private userService:UserService, private perfilService:PerfilService){}
+  constructor(private userService: UserService, private perfilService: PerfilService) { }
 
   ngOnInit(): void {
     const usuarioString = sessionStorage.getItem('usuario');
@@ -23,33 +24,50 @@ export class UserProfileComponent implements OnInit {
       // Si las publicaciones están almacenadas en el objeto usuario, adaptarlo aquí
       // this.publicaciones = this.usuario.publicaciones;
     } else {
-      window.location.href="/iniSesion";
+      window.location.href = "/iniSesion";
     }
     this.getPublicaciones();
     this.getSeguidoresSeguidos();
   }
-  
-  getPublicaciones(){
+
+  getPublicaciones() {
     this.perfilService.getPublicaciones(this.usuario.id).subscribe(data => {
       this.publicaciones = data;
-      console.log("publicaciones usuario",data);
-      this.numeroPublicaciones =  data.length;
+      console.log("publicaciones usuario", data);
+      this.numeroPublicaciones = data.length;
     })
 
   }
 
-  getSeguidoresSeguidos(){
-    this.userService.getSeguidores(this.usuario.id).subscribe(data=>{
+  getSeguidoresSeguidos() {
+    this.userService.getSeguidores(this.usuario.id).subscribe(data => {
       this.seguidores = data.length;
     })
-    this.userService.getSeguidos(this.usuario.id).subscribe(data=>{
+    this.userService.getSeguidos(this.usuario.id).subscribe(data => {
       this.seguidos = data.length;
     })
   }
 
-  cerrarSesion(){
+  abrirFormularioEdicion(): void {
+    this.mostrarFormularioEdicion = true;
+  }
+
+  cerrarFormularioEdicion(): void {
+    this.mostrarFormularioEdicion = false;
+  }
+
+  guardarCambios(): void {
+    // Aquí deberías enviar los cambios al servicio correspondiente
+    // Por ejemplo:
+    // this.userService.actualizarUsuario(this.usuario).subscribe(() => {
+    //   this.cerrarFormularioEdicion();
+    // });
+    this.cerrarFormularioEdicion();
+  }
+
+  cerrarSesion() {
     sessionStorage.removeItem('usuario');
-    window.location.href="/iniSesion";
+    window.location.href = "/iniSesion";
   }
 
   goToDetalles(id: number) {
@@ -58,6 +76,4 @@ export class UserProfileComponent implements OnInit {
     sessionStorage.setItem("publicaionSeleccionada", id_publi)
     window.location.href = `/fotoDetalle`;
   }
-  
 }
-
